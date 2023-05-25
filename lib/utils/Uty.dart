a9acfc72d81f34dd8f8b23ac027da1eba0dde357
Max <maxdiable.test@gmail.com>
2023-05-25 17:47:50 +0200

    add uty

---
+++ lib/utils/Uty.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+Future<void> showMyDialog(BuildContext context, {String title = 'Attenzione', String message = 'utf-8'}) async {
+  return showDialog<void>(
+    context: context,
+    barrierDismissible: false, // user must tap button!
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(title),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              //Text('This is a demo alert dialog.'),
+              Text(message),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          TextButton(
+            child: const Text('OK'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
